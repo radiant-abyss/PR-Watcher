6d2118660eeca90c4d43282697776612983751f5
RadiantAbyss <2016307096@qq.com>
2026-09-14 20:57:25 +0800

    Delete pr-watcher-git directory

--- pr-watcher-git/stop-watcher.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem Graceful stop: drop the stop marker; the watcher exits within 60 seconds.
-echo stop > "%~dp0watcher.stop"
-echo Requested stop. The watcher exits within a minute (see watcher.log).
-pause
